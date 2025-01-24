8ef672011723552afa138b690298f6df230c3454
NarciSource <jwch11@gmail.com>
2025-01-24 13:31:42 +0900

    style: 코드 정리

--- src/chat/chat-gateway.ts
+++ src/chat/chat-gateway.ts
@@ -12,7 +12,7 @@ import { Server, Socket } from 'socket.io';
  * - 원하는 만큼 참여자를 넣어 1:1 또는 1:N 모두 처리 가능
  */
 interface CreateRoomPayload {
-  hostId: string;          // 방을 생성한 사람 (옵션)
+  hostId: string; // 방을 생성한 사람 (옵션)
   participants: string[]; // 이 방에 들어갈 유저들의 userId 목록
 }
 
@@ -20,9 +20,9 @@ interface CreateRoomPayload {
  * 메시지 전송시 사용할 payload
  */
 interface SendMessagePayload {
-  roomId: string;   // 메시지를 전송할 방
+  roomId: string; // 메시지를 전송할 방
   senderId: string; // 보낸 사람
-  content: string;  // 메시지 내용
+  content: string; // 메시지 내용
 }
 
 @WebSocketGateway({
@@ -124,7 +124,7 @@ export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
    * 방 생성 이벤트 (1:1 ~ 1:N 모두 처리)
    *  - (예) socket.emit('create_room', {
    *        hostId: 'userA',
-   *        participantIds: ['userA','userB','userC'] 
+   *        participantIds: ['userA','userB','userC']
    *     });
    *  -> server가 랜덤 roomId를 만들어 roomMembersMap에 저장
    *  -> 해당 참여자들(userA,B,C)이 현재 소켓 연결 중이면 자동으로 room에 join
@@ -204,7 +204,7 @@ export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
     this.server.to(userId).emit('room_created', {
       roomId,
       participants,
-    })
+    });
   }
 
   /**
